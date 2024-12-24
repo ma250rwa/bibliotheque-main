7336f8f9ffcd2c29e38c3676994eb974126e1175
ma250rwa <ma250rwa@gmail.com>
2024-12-24 21:47:06 +0100

    main

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const auteurRoutes = require('./routes/auteurRoutes');
+const livreRoutes = require('./routes/livreRoutes');
+const { connexion } = require('./config/db');
+const {syncModels } = require('./models')
+
+const app = express();
+app.use(bodyParser.json());
+
+
+
+app.use('/api', livreRoutes);
+app.use('/api',auteurRoutes);
+
+const PORT= 4000;
+
+app.listen(PORT, async () =>{
+    console.log('app is running on port', PORT);
+    await connexion();
+})
+
+
+
+
+
